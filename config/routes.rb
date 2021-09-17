c27fffdb125b4de4b3552bcb0dd5a11a223ec345
Tzheng456 <Tzheng456@gmail.com>
2021-09-17 00:29:27 -0700

    add new about_controller, about view, and route to about page, link to about in top nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
+  resources :about, only: [:index]
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
