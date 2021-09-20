ff295f193e3a2ec5dd12def0c30d8658e9105d9e
Tzheng456 <Tzheng456@gmail.com>
2021-09-19 23:29:42 -0700

    fix spacing

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,6 @@ Rails.application.routes.draw do
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
 
-  
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
 
