e80cd6bbbe744c6d7b388b26eb28f4a14b627ad6
Tzheng456 <Tzheng456@gmail.com>
2021-09-19 23:29:25 -0700

    add users and sessions controllers, add user model, add users and sessions new.html.erb views, add routes for users and sessions

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,7 @@ Rails.application.routes.draw do
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
 
+  
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
 
