13a6be699b702401180f690ae59848c2b44153da
Tzheng456 <Tzheng456@gmail.com>
2021-09-20 19:14:24 -0700

    add default test for adding a product with 4 fields; add test for nil name field product

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,6 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    # validation tests/examples here
+
+    it 'should save a product with four fields set successfully' do
+      @category = Category.new(name: "TestCategory")
+      @product = Product.create(name: "name", price: 100, quantity: 1, category: @category)
+      expect(@product).to be_present
+    end
+
+    it 'should not have a nil value for name' do
+      @category = Category.new(name: "TestCategory")
+      @product = Product.create(name: nil, price: 100, quantity: 1, category: @category)
+      expect(@product.errors.full_messages).to include ("Name can't be blank")
+    end
+
   end
 end
