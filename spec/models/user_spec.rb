3e5a1eac6262d54bf19196bfe10e0ab25c4fa82c
Tzheng456 <Tzheng456@gmail.com>
2021-09-20 20:22:51 -0700

    implement initial test for successful user creation in user_spec, implement test for nil name value user create

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  
-  before do
-    @user = User.new(name: )
-  end
+  describe 'Validations' do
 
+    it 'should successfully create a user with all appropriate fields filled in' do
+      @user = User.create(name: "name", email:"1@test.com", password:"123456", password_confirmation: "123456")
+      expect(@user).to be_valid
+    end
+
+    it 'should not have a nil value for name' do
+      @user = User.create(name: nil, email:"1@test.com", password:"123456", password_confirmation: "123456")
+      expect(@user.errors.full_messages).to include ("Name can't be blank")
+    end
+  end
 end
